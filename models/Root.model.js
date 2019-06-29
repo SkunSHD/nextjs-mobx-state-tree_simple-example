b2d7ed29dce93890beeac02b9bb415f6b8046245
skunshd <skunshd@gmail.com>
2019-06-29 10:53:09 +0300

    Change indents

--- models/Root.model.js
+++ models/Root.model.js
@@ -1,30 +1,30 @@
-import {types} from "mobx-state-tree";
+import { types } from "mobx-state-tree";
 
 const RootModel = types
-    .model({
-        lastUpdate: types.Date,
-        light: false
-    })
-    .actions(self => {
-        let timer
-        function start () {
-            timer = setInterval(() => {
-                // mobx-state-tree doesn't allow anonymous callbacks changing data
-                // pass off to another action instead
-                self.update()
-            }, 1000)
-        }
+  .model({
+    lastUpdate: types.Date,
+    light: false
+  })
+  .actions(self => {
+    let timer
+    function start() {
+      timer = setInterval(() => {
+        // mobx-state-tree doesn't allow anonymous callbacks changing data
+        // pass off to another action instead
+        self.update()
+      }, 1000)
+    }
 
-        function update () {
-            self.lastUpdate = Date.now()
-            self.light = true
-        }
+    function update() {
+      self.lastUpdate = Date.now()
+      self.light = true
+    }
 
-        function stop () {
-            clearInterval(timer)
-        }
+    function stop() {
+      clearInterval(timer)
+    }
 
-        return { start, stop, update }
-    })
+    return { start, stop, update }
+  })
 
 export default RootModel
